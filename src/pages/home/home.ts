43afe8896fb9a76073f3512e05b5c715f6663686
alumno Secundaria <al3201@cedi.local>
2018-09-27 08:36:08 -0500

    page historia

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HistoriaPage } from '../historia/historia';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  historia = HistoriaPage;
+
 
   constructor(public navCtrl: NavController) {
 
+  }
+  clickhistoria(){
+    this.navCtrl.push(this.historia);
   }
 
 }
